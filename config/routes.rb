c4dd5802ac36ef14ecab5af17c0d629dea264016
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2009-08-23 19:14:31 -0400

    files generated by authenticated generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :users
+  map.resource :session
+
+  map.logout   '/logout', :controller => 'sessions', :action => 'destroy'
+  map.login    '/login', :controller => 'sessions', :action => 'new'
+  map.register '/register', :controller => 'users', :action => 'create'
+  map.signup   '/signup', :controller => 'users', :action => 'new'
   
   map.root :controller => 'pages'
 end
