a3cd445f180f8df9e8cec7646e50a963d1c0420f
Cloves Carneiro Jr <ccarneiroj@gmail.com>
2009-09-06 15:59:48 -0400

    added route for cart

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ ActionController::Routing::Routes.draw do |map|
 
   map.resources :users
   map.resource :session
+  map.resource :cart
 
   map.logout   '/logout', :controller => 'sessions', :action => 'destroy'
   map.login    '/login', :controller => 'sessions', :action => 'new'
