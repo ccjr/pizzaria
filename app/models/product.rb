c1d7be0f2c74f2d3c138ebfa92568832e7f8b45c
rbarazi <rbarazi@gmail.com>
2009-09-06 23:05:10 -0400

    Adding category support for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ActiveRecord::Base
   has_attached_file :image,
                     :styles => { :medium => "300x300>",
                                  :thumb => "100x100>" }
+                                 
+  CATEGORIES = ["Pizza", "Drink", "Other"]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090907024858) do
+ActiveRecord::Schema.define(:version => 20090907025851) do
 
   create_table "order_items", :force => true do |t|
     t.integer  "order_id"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(:version => 20090907024858) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.string   "category"
+    t.decimal  "price"
   end
 
   create_table "users", :force => true do |t|
