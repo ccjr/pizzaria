d770ff331471f533683ca2dd6c9412bad2e44222
rbarazi <rbarazi@gmail.com>
2010-04-11 16:22:32 -0400

    Adding the clean beta3 install

---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.cookie_secret = '4ed42aabc72887d7e15070ff949ef3c70d40cb15d2ea549c586b6c3ea1c4c3b02d1376a8d96c89c3931b92416eeb081564ce913970b4cd286c3559cde112dda4'
