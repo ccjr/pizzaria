d1ecd4fc4abcde3d285b0d274e9584c181de77e1
rbarazi <rbarazi@gmail.com>
2010-04-13 19:52:05 -0400

    Updates to cart controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,4 @@
 class CartsController < ApplicationController
+  before_filter :authenticate_user!, :only => :checkout
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Pizzaria::Application.routes.draw do |map|
   resources :products
   
   resource :cart do
+    get :checkout
+    post :process
     resources :items 
   end
   
