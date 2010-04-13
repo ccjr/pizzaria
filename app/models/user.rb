835db5379e232367dbf73952617a7dada7b7ffb4
rbarazi <rbarazi@gmail.com>
2010-04-13 13:06:02 -0400

    Adding seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :lockable, :timeoutable and :activatable
-  devise :database_authenticatable, :registerable, :confirmable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+admin = User.create(:email => "admin@example.com", :password => "secret", :password_confirmation => "secret", :admin => true)
+
+products = Product.create([
+  {:name => "New York Pizza", :description => "Salami, tomato souce and mozzarella cheese", :category => "Pizza", :price => 12},
+  {:name => "Margherita Pizza", :description => "Tomato, sliced mozzarella, basil and extra-virgin olive oil", :category => "Pizza", :price => 10},
+  {:name => "French Fries", :description => "Golden fried potatos", :category => "Other", :price => 5},
+  {:name => "Coca-cola Zero ", :description => "Coca-cola with zero calories", :category => "Drink", :price => 2}
+])
